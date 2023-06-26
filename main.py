cb77613cb5ea0f8f815555e686db14c5633092f4
damianokpala111@gmail.com <damianokpala111@gmail.com>
2023-06-26 16:04:05 +0100

    set-up-for-higher-lower

---
+++ main.py
@@ -0,0 +1,22 @@
+# import the logo
+
+# print logo
+
+# generate first random for data 
+
+
+# print data for compare
+
+
+# print logo "vs"
+
+#priprint data for against
+
+# ask user for input  "Who has more followers? Type "A" or "B"
+
+# condition
+
+# if a: check if a is greater for the value in data then add score by 1 and print "You're correct, your score is"
+
+
+# else: Output: Sorry, that's wrong. Final score is
